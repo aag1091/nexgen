b20ec5dd66a84f96e6b9ecbec10ce6762c4864d1
aag1091 <aag1091@aag1091-M68M-S2P.(none)>
2013-05-12 15:29:29 +0530

    configured rails + test envionment

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Nexgen::Application.config.secret_token = '3ad2005052a314bdd55690ecf8f9738d1c7cad9288932ff4cd091b4a56b9e8c265b84e49cc608af9035d76104538ca65c26aac91dd1f0a882ee4f175c3e35be1'
