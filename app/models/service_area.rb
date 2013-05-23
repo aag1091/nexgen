a289cf6b588cdb20fe4b2ffbceecd8303f3da388
aag1091 <aag1091@aag1091-M68M-S2P.(none)>
2013-05-23 22:16:49 +0530

    Added changes for adding tabs

--- app/models/service_area.rb
+++ app/models/service_area.rb
@@ -1,3 +1,4 @@
 class ServiceArea < ActiveRecord::Base
-  attr_accessible :is_on_home, :description, :is_active, :name, :short_desc
+  attr_accessible :is_on_home, :description, :is_active, :name, :short_desc, :service_ids
+  has_many :services
 end
